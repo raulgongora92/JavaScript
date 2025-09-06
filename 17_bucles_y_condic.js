c579ef476c8f3a68203164b4c6d3f67886a26aad
Raul Gongora <raulgongora92@gmail.com>
2025-09-05 22:13:43 -0500

    Bucle for + condicionales

---
+++ 17_bucles_y_condic.js
@@ -0,0 +1,35 @@
+// Bucle For + Condicional If-Else y Switch
+
+for (var i =  1; i <= 10; i++) {
+    if (i == 1){
+        console.log('Gold Medal');
+    }else if (i == 2){
+        console.log('Silver medal');
+    }else if (i == 3){
+        console.log('Bronze medal');
+    }else{
+        console.log(i);
+    }
+}
+
+console.log('----------------------');
+
+// Bucle For + Condicional Switch
+
+for (var i =  1; i <= 10; i++) {
+
+    switch(i) {
+        case 1:
+            console.log('Gold Medal');
+            break;
+        case 2:
+            console.log('Silver Medal');
+            break;
+        case 3:
+            console.log('Bronze Medal');
+            break;
+        default:
+            console.log(i);
+    }
+    
+}
