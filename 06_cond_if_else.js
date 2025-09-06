8bca9dfa76866b2252c0db2631dafc203659579d
Raul Gongora <raulgongora92@gmail.com>
2025-09-05 21:12:05 -0500

    Condicional if, else

--- 06_cond_if_else.js
+++ 06_cond_if_else.js
@@ -1,4 +1,9 @@
+// Condicionales if else
+// Si la condición es verdadera, se ejecuta el bloque de código dentro del if
+// Si la condición es falsa, se ejecuta el bloque de código dentro del else
+
 var result = 30;
+
 if (result > 40) {
     console.log("Has Aprobado");
 } else {
