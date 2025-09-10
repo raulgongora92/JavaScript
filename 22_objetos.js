b6ea27b65170b644d39ab028282b28f0e4fda4f7
Raul Gongora <raulgongora92@gmail.com>
2025-09-10 07:31:49 -0500

    Introducción a los objetos

---
+++ 22_objetos.js
@@ -0,0 +1,40 @@
+// objetos
+
+var storeManager = {
+    rangeTilesPerTurn: 4,
+    socialSkills: 50,
+    streetSmarts: 50,
+    health: 30,
+    specialAbility: "finding business opportunities",
+    greeting: "Let's make some money"
+}
+
+var assistantManager = {
+    movement: 3,
+    socialSkills: 30,
+    streetSmarts: 30,
+    health: 40
+}
+
+console.log(storeManager, assistantManager);
+console.log("-------------------");
+
+// Agregar una propiedad
+storeManager.nextAchievement = "open a new store";
+assistantManager.nextAchievement = "get promoted";
+
+console.log(storeManager, assistantManager);
+console.log("-------------------");
+
+// Eliminar una propiedad
+delete storeManager.socialSkills;
+delete assistantManager.streetSmarts;
+
+console.log(storeManager, assistantManager);
+console.log("-------------------");
+
+// Modificar una propiedad
+storeManager.rangeTilesPerTurn = 5;
+assistantManager.movement = 4;  
+console.log(storeManager, assistantManager);
+
