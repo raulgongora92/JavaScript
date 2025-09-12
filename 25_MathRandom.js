5a75dbfac54e679091513568043daa962d2d7dab
Raul Gongora <raulgongora92@gmail.com>
2025-09-12 07:55:20 -0500

    metodo math random, numeros aleatorios

---
+++ 25_MathRandom.js
@@ -0,0 +1,9 @@
+// genera numeros aleatorios entre 0 y 0.99
+var decimal = Math.random();
+
+console.log(decimal);
+
+// genera numeros aleatorios entre 0 y 10
+var entero = Math.trunc(decimal * 10);
+
+console.log(entero);
