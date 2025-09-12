73b1f7567f34c08d8b4e46c99ef69d8a5c8c0650
Raul Gongora <raulgongora92@gmail.com>
2025-09-12 08:08:29 -0500

    ejemplo del metodo math random y ceil

---
+++ 27_RandomInteger.js
@@ -0,0 +1,7 @@
+// Generar un numero aleatorio entre 0 y 10
+var decimal = Math.random() * 10;
+console.log(decimal);
+
+// Redondear el numero al entero mas cercano
+var rounded = Math.ceil(decimal);
+console.log(rounded);
