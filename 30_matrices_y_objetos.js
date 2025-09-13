3290c39c138b9e6fea36086a114aecd5efd9d618
Raul Gongora <raulgongora92@gmail.com>
2025-09-13 08:30:24 -0500

    Ejemplo de matrices y objetos

---
+++ 30_matrices_y_objetos.js
@@ -0,0 +1,18 @@
+var ropa = [];
+ropa.push("sueteres");
+ropa.push("jeans");
+ropa.push("pantalonetas");
+ropa.push("camisas");
+ropa.push("medias");
+
+ropa.pop();
+
+ropa.push("bufandas");
+
+console.log(ropa[2]);
+
+var favCar = {};
+favCar.color = "Negro";
+favCar.convertible = true;
+
+console.log(favCar);
