cad6645aa53dea49e33e954b4cc4ed437db04a12
Raul Gongora <raulgongora92@gmail.com>
2025-11-08 15:31:19 -0500

    Variables

---
+++ 05_variables.js
@@ -0,0 +1,26 @@
+// Variables
+
+// EPara crear una variable podemos usar la palabra reservada let y le damos un nombre a la variable. Por ejemplo:
+
+let numero = 3;
+console.log(numero + 6);
+
+let welcomeText = 'Hola'
+let isCool = true
+
+// Constantes const
+
+//Las constantes son variables que no pueden ser reasignadas. Para crear una constante, usamos la palabra reservada const:
+
+const PI = 3.1415
+console.log(PI);
+
+// Si intentas reasignar el valor de una constante, obtendrás un error:
+
+//PI = 3 // -> TypeError: Assignment to constant variable.
+
+// Variables var
+
+//En JavaScript, también podemos crear variables usando la palabra reservada var. Es la forma más antigua y es normal que encuentres muchos tutoriales que lo usen. Sin embargo, a día de hoy, no es recomendable usar var ya que tiene comportamientos extraños que pueden causar errores en tu código.
+
+// Es importante tener en cuenta que los nombres de las variables son sensibles a las mayúsculas y minúsculas, lo que significa que miVariable y mivariable son dos variables diferentes en JavaScript.
