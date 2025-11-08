929f4816f428bb1991f7178eddc2e2fa1df6ecd9
Raul Gongora <raulgongora92@gmail.com>
2025-11-08 14:33:21 -0500

    Operadores Aritmeticos

---
+++ 02_operadAritmeticos.js
@@ -0,0 +1,27 @@
+// Operadores aritméticos
+
+//Con los números, puedes usar los operadores aritméticos para realizar operaciones matemáticas. En JavaScript tenemos los siguientes operadores aritméticos:
+
+//+: suma
+//-: resta
+//*: multiplicación
+///: división
+//%: módulo (resto de la división)
+//**: exponente
+
+//Al usar los operadores aritméticos, el resultado siempre será un número. Por ejemplo:
+
+2 + 2 // 4
+4 - 2 // 2
+3 * 2 // 6
+2 / 2 // 1
+2 % 2 // 0
+3 ** 3 // 27
+
+//Al igual que las matemáticas, las operaciones siguen un orden de precedencia. Por ejemplo, si queremos calcular el resultado de 2 + 2 * 3, primero se multiplicará 2 * 3 y luego se sumará 2 + 6. El resultado será 8.
+
+//También puedes usar paréntesis para cambiar el orden de las operaciones.
+
+2 + 2 * 3 // 8
+(2 + 2) * 3 // 12
+//Más adelante le dedicaremos una clase entera a los BigInt, un tipo de dato similar a Number pero pensado para trabajar con números grandes.
