53abe750529bea0418ed1c1f01f797b200f75f44
Raul Gongora <raulgongora92@gmail.com>
2025-11-08 14:25:11 -0500

    Tipos de Datos

---
+++ 01_tipoDeDatos.js
@@ -0,0 +1,47 @@
+// Tipos de datos en JavaScript
+
+//En JavaScript tenemos 9 tipos de datos que se dividen en dos grandes grupos: primitivos o no primitivos. 
+
+//Dentro de los tipos primitivos tenemos 7 tipos de datos:number, string, boolean, null, undefined, symbol, bigint
+
+// Números
+
+//Los números (tipo number) son los datos más básicos que podemos representar en JavaScript. En JavaScript, no hay una diferencia entre números enteros y números decimales, todos los números son de tipo number:
+
+7
+3.14
+19.95
+2.998e8 - 1
+
+//Cadenas de texto
+
+//La cadena de texto (tipo string) es otro tipo de dato muy común. En JavaScript, las cadenas de texto se representan entre comillas simples, dobles o acentos graves:
+
+'Estás aprendiendo JavaScript'
+
+'JavaScript te va a gustar'`Esto es una cadena de texto`
+//Las comillas simples y dobles funcionan igual, pero al usar acentos graves podemos escribir cadenas de texto que ocupen varias líneas:
+
+`Esto es una cadena de texto
+que ocupa varias líneas. Puedes escribir
+tantas líneas como quieras`
+//En clases posteriores, veremos cómo funcionan, algunos métodos interesantes y sus diferencias.
+
+//Concatenación
+
+//Para unir dos cadenas de texto, podemos usar el operador +:
+
+'Estás aprendiendo ' + 'JavaScript' // 'Estás aprendiendo JavaScript'
+//Como ves, el operador + de concatenación de cadenas de texto es visualmente el mismo que el operador + de suma de números. El operador + funciona de forma diferente dependiendo del tipo de dato que estemos usando.
+
+//Booleanos
+
+//Los booleanos representa sólo dos valores: true (verdadero) o false (falso). Por ejemplo:
+
+//¿La luz está encendida (true) o apagada (false)?
+//¿Está lloviendo (true) o no está lloviendo (false)?
+//¿Está el usuario logueado (true) o no está logueado (false)?
+//Estos son ejemplos de preguntas que podemos responder con un valor booleano.
+
+true
+false
